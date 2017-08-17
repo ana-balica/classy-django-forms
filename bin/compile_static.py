d589513f1ee783e9f6d81bfdd3b57031d3f6f38f
Ana Balica <ana.balica@gmail.com>
2017-08-17 14:17:33 -0700

    Placeholder for index generation and compiling static files

---
+++ bin/compile_static.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+def main():
+    print 'compile static'
+
+
+if __name__ == '__main__':
+    main()
---
+++ bin/index_generator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+def main():
+    print "index generator"
+
+
+if __name__ == '__main__':
+    main()
