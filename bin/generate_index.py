b058d00ad1ef55c97fe0142a20e09784eefd480a
Ana Balica <ana.balica@gmail.com>
2017-08-20 10:45:08 -0700

    Group by module when extracting classes

--- bin/generate_index.py
+++ bin/generate_index.py
@@ -2,28 +2,12 @@
 import os
 import json
 
-from itertools import groupby
-
 from cdf.config import VERSION
 from cdf.inspector import get_klasses
 
 
-def sort_by_module(klasses):
-    return sorted(klasses, key=lambda klass: klass.__module__)
-
-
-def group_by_module(klasses):
-    klasses_by_module = {}
-    for key, value in groupby(klasses, lambda klass:  klass.__module__):
-        klasses_by_module[key] = list(map(lambda x: x.__name__, list(value)))
-
-    return klasses_by_module
-
-
 def main():
-    all_klasses = get_klasses()
-    klasses = sort_by_module(all_klasses)
-    klasses_by_module = group_by_module(klasses)
+    klasses = get_klasses()
 
     # Load existing Django forms spec
     forms_api_spec = {}
@@ -32,7 +16,7 @@ def main():
             forms_api_spec = json.loads(f.read())
 
     # Update the spec and write back
-    forms_api_spec[VERSION] = klasses_by_module
+    forms_api_spec[VERSION] = klasses
     with open('.klasses.json', 'w') as f:
         json.dump(forms_api_spec, f, indent=2)
 
--- cdf/inspector.py
+++ cdf/inspector.py
@@ -4,13 +4,16 @@ from django import forms
 def get_klasses():
     """
     Get all the public classes from the Django modules of interest within forms.
+
+    :returns: dict of the form {'module_name': ['classA', 'classB', 'classC']}
     """
     # TODO: update the list of modules
     modules = [forms.fields, forms.widgets]
-    klasses = []
+    klasses = {}
 
     for module in modules:
+        module_name = module.__name__
+        klasses[module_name] = []
         for klass_name in module.__all__:
-            klass = getattr(module, klass_name)
-            klasses.append(klass)
+            klasses[module_name].append(klass_name)
     return klasses
