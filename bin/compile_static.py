c61bdd0ad98aed71f531cf9afe2dbdbfc1b519d2
Ana Balica <ana.balica@gmail.com>
2017-08-20 10:50:23 -0700

    Compile a landing page with only the nav bar

--- bin/compile_static.py
+++ bin/compile_static.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
+import os
+
+from cdf.config import DJANGO_VERSIONS, VERSION
+from cdf.inspector import get_klasses
+from cdf.renderers import (
+    IndexPageRenderer,
+)
+
 
 def main():
-    print('compile static')
+    klasses = get_klasses()
+
+    out_folder = 'public'
+    for module, _ in klasses.items():
+        target_path = os.path.join(out_folder, VERSION, module)
+        os.makedirs(target_path, exist_ok=True)
+
+    renderer = IndexPageRenderer(klasses)
+    index_path = os.path.join(out_folder, VERSION, 'index.html')
+    renderer.render(index_path)
 
 
 if __name__ == '__main__':
