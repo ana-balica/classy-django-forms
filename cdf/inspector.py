8f70a0dc430cf25e53f969bb3bab114db61add8d
Ana Balica <ana.balica@gmail.com>
2017-08-22 16:41:16 -0700

    Basic inspector, method to get versions in which a class exists

--- cdf/inspector.py
+++ cdf/inspector.py
@@ -1,3 +1,5 @@
+import json
+
 from django import forms
 
 
@@ -17,3 +19,25 @@ def get_klasses():
             klass = getattr(module, klass_name)
             klasses[module].append(klass)
     return klasses
+
+
+class Inspector:
+
+    def __init__(self, klass):
+        self.klass = klass
+        self.klass_name = klass.__name__
+        self.module_name = klass.__module__
+
+    def get_available_versions(self):
+        """
+        Return a list of versions in which the class is present.
+        """
+        with open('.klasses.json', 'r') as f:
+            klass_versions = json.loads(f.read())
+
+        return [
+            version
+            for version in klass_versions
+            if self.module_name in klass_versions[version] and
+            self.klass_name in klass_versions[version][self.module_name]
+        ]
