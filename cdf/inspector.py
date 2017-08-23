7d9626eb6d8c2bab8965a441e952c4ac5c65c61f
Ana Balica <ana.balica@gmail.com>
2017-08-22 19:13:55 -0700

    Get class ancestors and descendants

--- cdf/inspector.py
+++ cdf/inspector.py
@@ -1,3 +1,4 @@
+import types
 import json
 
 from django import forms
@@ -27,6 +28,31 @@ class Inspector:
         self.klass = klass
         self.klass_name = klass.__name__
         self.module_name = klass.__module__
+
+    def get_ancestors(self):
+        """
+        Return a list of class ancestors using the method resolution order.
+        """
+        ancestors = []
+        for ancestor in type.mro(self.klass):
+            if ancestor is not object:
+                ancestors.append(ancestor)
+        return ancestors
+
+    def get_direct_ancestors(self):
+        return self.klass.__bases__
+
+    def get_descendants(self):
+        """
+        Get the list of classes that inherit from the target class.
+        """
+        descendants = []
+        klasses_by_module = get_klasses()
+        for module, klasses in klasses_by_module.items():
+            for klass in klasses:
+                if klass != self.klass and issubclass(klass, self.klass):
+                    descendants.append(klass)
+        return descendants
 
     def get_available_versions(self):
         """
