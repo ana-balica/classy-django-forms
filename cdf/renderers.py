f65dbeeb5a6e1b54f07ee34778576d7af21a1b93
Ana Balica <ana.balica@gmail.com>
2017-08-22 19:20:46 -0700

    Transfer renderer for details page and put available stuff in the context
    Leave it like this for now, later on rethink the API for rendering
    the pages.

--- cdf/renderers.py
+++ cdf/renderers.py
@@ -1,4 +1,5 @@
 from cdf.config import DJANGO_VERSIONS, VERSION
+from cdf.inspector import Inspector
 from cdf.jinja_utils import template_env
 
 
@@ -26,3 +27,25 @@ class BasicPageRenderer(object):
         }
 
 
+class DetailsPageRenderer(BasicPageRenderer):
+
+    def __init__(self, klasses, klass):
+        super(DetailsPageRenderer, self).__init__(klasses)
+        self.klass = klass
+        self.inspector = Inspector(klass)
+
+    def get_context(self):
+        context = super(DetailsPageRenderer, self).get_context()
+        available_versions = self.inspector.get_available_versions()
+
+        context['other_versions'] = [
+            version
+            for version in context['other_versions']
+            if version in available_versions
+        ]
+        context['this_klass'] = self.klass
+        context['this_klass_name'] = self.klass.__name__
+        context['ancestors'] = self.inspector.get_ancestors()
+        context['direct_ancestors'] = self.inspector.get_direct_ancestors()
+        context['descendants'] = self.inspector.get_descendants()
+        return context
