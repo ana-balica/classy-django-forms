a7afe12e241ee8f6ca8b85850ff43b777220ec62
Ana Balica <ana.balica@gmail.com>
2017-08-23 18:16:02 +0100

    Add basic django config to be able to manipulate form classes

---
+++ cdf/__init__.py
@@ -0,0 +1,13 @@
+import django
+from django.conf import settings
+
+settings.configure(
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', 
+            'NAME': ':memory:'
+        }
+    },
+)
+
+django.setup()
