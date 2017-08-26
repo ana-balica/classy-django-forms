bca2fbe2572f9b735194d130b9ac361f01b7d8b5
Ana Balica <ana.balica@gmail.com>
2017-08-26 12:16:13 +0100

    Add class url context function

--- cdf/jinja_utils.py
+++ cdf/jinja_utils.py
@@ -1,11 +1,24 @@
+import os
+
 from jinja2 import (
+    contextfunction,
     select_autoescape,
     Environment,
     FileSystemLoader,
 )
+
+from cdf.config import VERSION, EXACT_VERSION
 
 
 template_env = Environment(
     loader=FileSystemLoader("templates"),
     autoescape=select_autoescape(['html', 'xml'])
 )
+
+
+@contextfunction
+def get_klass_url(context, klass, version=VERSION):
+    return os.path.join('/', version, klass.__module__, klass.__name__ + '.html')
+
+
+template_env.globals['get_klass_url'] = get_klass_url
