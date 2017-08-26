1f88160562701e1dd478caedadac70a16c9d7a8f
Ana Balica <ana.balica@gmail.com>
2017-08-26 12:43:47 +0100

    Add documentation link for each class

--- cdf/jinja_utils.py
+++ cdf/jinja_utils.py
@@ -35,6 +35,18 @@ def get_klass_docs(context, klass):
     return ''
 
 
+@contextfunction
+def get_doc_link(context, klass, version=VERSION):
+    base_url = 'https://docs.djangoproject.com/en/{version}/ref/forms/{module}/#{klass}'
+    short_module = klass.__module__.rsplit('.', 1)[-1]
+    return base_url.format(
+        version=version,
+        module=short_module,
+        klass=klass.__name__.lower()
+    )
+
+
 template_env.globals['get_klass_url'] = get_klass_url
 template_env.globals['get_version_url'] = get_version_url
 template_env.globals['get_klass_docs'] = get_klass_docs
+template_env.globals['get_doc_link'] = get_doc_link
