8dd360b41d3e5892ab82dbf11d6cc0508845148a
Robert Huselius <robert@huseli.us>
2023-04-14 17:19:21 +0200

    Monkey patching collections module for newer Python versions and older Django ones

--- src/cdf/__init__.py
+++ src/cdf/__init__.py
@@ -1,3 +1,7 @@
+import collections
+import sys
+import typing
+
 import django
 from django.conf import settings
 
@@ -9,5 +13,14 @@ settings.configure(
         }
     },
 )
+
+# Monkey patching needed for newer Python versions and older Django ones:
+if not hasattr(collections, "Mapping"):
+    collections.Mapping = typing.Mapping
+if not hasattr(collections, "Iterator"):
+    collections.Iterator = typing.Iterator
+if not hasattr(collections, "Sequence"):
+    collections.Sequence = typing.Sequence
+sys.modules["collections"] = collections
 
 django.setup()
